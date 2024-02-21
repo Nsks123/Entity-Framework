b23226db85a86ebdf446e9dfb94477b9a4f633e7
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-21 15:18:18 +0530

    [sunil] Entity framework

---
+++ Data/Connection_Context.cs
@@ -0,0 +1,26 @@
+﻿using Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework.Data
+{
+    public class Connection_Context : DbContext
+    {
+        public DbSet< Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; }=null!;
+
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=BoosStoreAPI:Integrated Security=True");
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Entity
+{
+    public class Entity
+    {
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}
